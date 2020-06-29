36a9fa48317dc750862f84779c009790e593328e
Richard Darst <rkd@zgib.net>
2020-06-30 00:29:23 +0300

    sphinx_lesson/sphinx: Transform code fences to commonmark

--- sphinx_lesson/sphinx.py
+++ sphinx_lesson/sphinx.py
@@ -1,11 +1,15 @@
 # pylint: disable=E701
 
 import os
+import re
 
 from docutils import nodes
 from docutils.parsers.rst.directives.admonitions \
   import Admonition as AdmonitionDirective
 from sphinx.util.docutils import SphinxDirective
+from sphinx.util.logging import getLogger
+
+LOG = getLogger('sphinx-lesson')
 
 
 # class challenge(nodes.admonition):
@@ -74,6 +78,40 @@ def init_static_path(app):
     #print('sphinx_lesson static path:', static_path)
     app.config.html_static_path.append(static_path)
 
+def transform_code_fences(app, docname, source):
+    """Transform a code fence when read.
+
+    Changes:
+    ```
+    blah
+    ```
+    {: output}
+
+    Into this:
+    ```{output}
+    blah
+    ```
+    """
+    if not app.config.sphinx_lesson_transform_code_fences:
+        return
+    print(docname)
+    content = source[0]
+    LOG.debug(content)
+    code_fence_re = re.compile(
+        r'^(?P<before>```.*?)(?P<content>\n.*?)(?P<after>^``` ?\n)(?:\{:(?P<class>[^}\n]+)\}$)?',
+        re.DOTALL|re.MULTILINE,
+)
+    def sub_fence(m):
+        if m.group('class'):
+            print("matched:", m.group(0))
+            print("class:", m.group('class'))
+            return m.group('before') + '{%s}'%m.group('class').strip() + m.group('content') + m.group('after')
+        else:
+            return m.group(0)
+
+    newcontent = code_fence_re.sub(sub_fence, content)
+    LOG.debug(newcontent)
+    source[0] = newcontent
 
 def setup(app):
     "Sphinx extension setup"
@@ -94,3 +132,7 @@ def setup(app):
     # Hint is from https://github.com/choldgraf/sphinx-copybutton/blob/master/sphinx_copybutton/__init__.py  # pylint: ignore=E501
     app.connect('builder-inited', init_static_path)
     app.add_css_file("sphinx_coderefinery.css")
+    #app.add_css_file("lesson.css")
+    # Code frence transformation
+    app.add_config_value('sphinx_lesson_transform_code_fences', True, 'env')
+    app.connect('source-read', transform_code_fences)
