e960da67191483de435c74c66e8bfe6a8ff251a7
Richard Darst <rkd@zgib.net>
2020-07-12 00:25:12 +0300

    setup: Include _static data

--- setup.py
+++ setup.py
@@ -24,6 +24,9 @@ setuptools.setup(name='sphinx_lesson',
       author='Richard Darst',
       #author_email='',
       packages=['sphinx_lesson'],           # packages
+      package_data={
+          "sphinx_lesson": ['_static/*'],
+          },
       #py_modules=["nbscript"],   # single modules
       keywords='sphinx-extension',
       #python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*,",
