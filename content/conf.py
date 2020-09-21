83e627a3c83e397ddbd8aa52eae1ed5dc3643e37
Richard Darst <rkd@zgib.net>
2020-09-21 22:24:11 +0300

    Test of the sphinx-book-theme
    - This isn't necessarily to be merged, but we can see how it looks
      with the different branch rendering.
    - This branch will continue to be rebased forward

--- content/conf.py
+++ content/conf.py
@@ -77,7 +77,13 @@ exclude_patterns = ['README*', '_build', 'Thumbs.db', '.DS_Store',
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_book_theme'
+html_theme_options = {
+    "repository_url": "https://github.com/coderefinery/sphinx-lesson/",
+    "use_repository_button": True,
+    "use_edit_page_button": True,
+    "use_issues_button": True,
+    }
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
